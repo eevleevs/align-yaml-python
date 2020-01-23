3367981287d1bd1b46107f27ac601955db8e7108
Giulio Malventi <giulio.malventi@gmail.com>
2020-01-23 10:06:43 +0100

    Update align_yaml.py

--- align_yaml.py
+++ align_yaml.py
@@ -5,6 +5,6 @@ def align_yaml(str, pad=0):
     longest = max([len(i) for i in props]) + pad
     return ''.join([i+'\n' for i in map(lambda str:
             re.sub(r'^(\s*.+?[^:#]: )\s*(.*)', lambda m:
-                    m.group(1) + ''.ljust(longest - len(m.group(1)) + 1) + m.group(2),
+                    m.group(1) + ''.ljust(longest-len(m.group(1))+1) + m.group(2),
                 str, re.MULTILINE)
         , str.split('\n'))])
