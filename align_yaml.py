729a11b848bf8a690edf31f6ebdeb4908d2f6676
Giulio Malventi <giulio.malventi@gmail.com>
2020-01-26 19:46:44 +0100

    Added colon alignment

--- align_yaml.py
+++ align_yaml.py
@@ -1,10 +1,10 @@
 import re
 
-def align_yaml(str, pad=0):
+def align_yaml(str, pad=0, aligned_colons=False):
     props = re.findall(r'^\s*[\S]+:', str, re.MULTILINE)
     longest = max([len(i) for i in props]) + pad
     return ''.join([i+'\n' for i in map(lambda str:
-            re.sub(r'^(\s*.+?[^:#]: )\s*(.*)', lambda m:
-                    m.group(1) + ''.ljust(longest-len(m.group(1))+1) + m.group(2),
+            re.sub(r'^(\s*.+?[^:#]): \s*(.*)' if aligned_colons else r'^(\s*.+?[^:#]: )\s*(.*)', lambda m:
+                    m.group(1) + ''.ljust(longest-len(m.group(1))+(-1-pad if aligned_colons else 1)) + (':'.ljust(pad+1) if aligned_colons else '') + m.group(2),
                 str, re.MULTILINE)
         , str.split('\n'))])
