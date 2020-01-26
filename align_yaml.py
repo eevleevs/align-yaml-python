1ced4f8e17675e1996740a51c95612802c430753
Giulio Malventi <giulio.malventi@gmail.com>
2020-01-26 19:54:15 +0100

    Split code for alignment
    Probably more performant, even if it introduces some repetition.

--- align_yaml.py
+++ align_yaml.py
@@ -3,8 +3,15 @@ import re
 def align_yaml(str, pad=0, aligned_colons=False):
     props = re.findall(r'^\s*[\S]+:', str, re.MULTILINE)
     longest = max([len(i) for i in props]) + pad
-    return ''.join([i+'\n' for i in map(lambda str:
-            re.sub(r'^(\s*.+?[^:#]): \s*(.*)' if aligned_colons else r'^(\s*.+?[^:#]: )\s*(.*)', lambda m:
-                    m.group(1) + ''.ljust(longest-len(m.group(1))+(-1-pad if aligned_colons else 1)) + (':'.ljust(pad+1) if aligned_colons else '') + m.group(2),
-                str, re.MULTILINE)
-        , str.split('\n'))])
+    if aligned_colons:
+        return ''.join([i+'\n' for i in map(lambda str:
+                re.sub(r'^(\s*.+?[^:#]): \s*(.*)', lambda m:
+                        m.group(1) + ''.ljust(longest-len(m.group(1))-1-pad) + ':'.ljust(pad+1) + m.group(2),
+                    str, re.MULTILINE)
+            , str.split('\n'))])
+    else:
+        return ''.join([i+'\n' for i in map(lambda str:
+                re.sub(r'^(\s*.+?[^:#]: )\s*(.*)', lambda m:
+                        m.group(1) + ''.ljust(longest-len(m.group(1))+1) + m.group(2),
+                    str, re.MULTILINE)
+            , str.split('\n'))])
